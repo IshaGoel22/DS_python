b3837a5af6351c47c599a393ab55dd7eecdf947a
ISHA <65284360+IshaGoel22@users.noreply.github.com>
2021-01-17 21:02:12 +0530

    Create DS_1.py

---
+++ DS_1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 19:53:57 2021
+
+@author: ISHA
+"""
+
+"""
+Created on 17-01-21
+by isha goel
+"""
+print('hi')
+print("Hi I am isha")
+"""
+by default new line
+"""
+print('data science',end='')
+print(" course")
+"""
+sep='\n' new line
+sep='-' - btw each word
+end='' to start next line after finishing first
+end='\t' space b/w first and second line
+"""
+print('Hi','welcome')
+
+import pandas
+
+pandas.__version__
+
+!pip install pandas
+!pip list     #for list of version
+!pip install --upgrade pandas
