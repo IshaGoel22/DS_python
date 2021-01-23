4f135e5106eba9448d43b45defcffcf57ab23d48
ISHA <65284360+IshaGoel22@users.noreply.github.com>
2021-01-23 19:36:32 +0530

    Create DS_2.py

---
+++ DS_2.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 23 15:35:45 2021 ....17jan
+
+@author: ISHA
+"""
+#static input
+a = 10
+b =1.5
+c = True
+country = 'India'
+'''
+no need to specify type of variable
+like in other programming languages
+
+'''
+
+country1 = input()                      
+print(country1)
+
+country2 = input("live--> " )
+print("--> ",country2)
+
+print("i live in {0} and want to go {1}".format(country1,country2))
+print("i live in {0} and want to go {1}".format(country,country1))
+print("i live in {1} and want to go {0}".format(country1,country2))
+'''
+{0} - .format(xyz)
+in same line {0}.... {1}  .format(0,1)
+'''
+#type casting or type conversion
+
