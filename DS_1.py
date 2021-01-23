a6fc118d95d4497178710c8b3201c523c8cf3cc0
ISHA <65284360+IshaGoel22@users.noreply.github.com>
2021-01-23 19:36:18 +0530

    Update DS_1.py

--- DS_1.py
+++ DS_1.py
@@ -13,17 +13,23 @@ print('isha goel')
 """
 by default new line
 """
-print('data science',end='\t')
+print('data science',end=' ')
 print('course')
 """
-sep='\n' new line
+sep='\n' new line after each word
 sep='-' - btw each word
 end='' to start next line after finishing first
 end='\t' space b/w first and second line printed in same line
 """
-print('Hi','welcome',sep='-')
-print('hello')
+print('Hi','welcome')
+print('hello','hyyy',sep='-')
 
+''' 
+!pip linked to 
+pypi.org  site
+for instllation in python
+
+'''
 import pandas
 pandas.__version__
 !pip install pandas
@@ -48,8 +54,11 @@ pd.__version__
 # install Packages
 
 !pip install pandas
-!conda install numpy
+!conda install numpy   '''(install numpy from anaconda)'''
 
 #Search
 !conda search scipy
 !conda list
+
+#pip version
+!pip --version   
